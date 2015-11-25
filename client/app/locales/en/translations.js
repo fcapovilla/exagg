42919e56a280e9382faa588d02ce1dd439b9058d
Frédérick Capovilla <caposite@gmail.com>
2015-11-24 23:04:57 -0500

    Add ember-i18n and translations.

---
+++ client/app/locales/en/translations.js
@@ -0,0 +1,33 @@
+export default {
+  'feed': {
+    'markRead': 'Mark as read',
+    'markUnread': 'Mark as unread',
+    'edit': 'Edit',
+    'delete': 'Delete',
+    'url': 'URL',
+    'folder': 'Folder',
+    'reset': 'Reset feed',
+    'pshb': {
+      'hub': 'Hub',
+      'topic': 'Topic',
+      'expiration': 'Expiration',
+      'status': 'Status'
+    }
+  },
+
+  'folder': {
+    'markRead': 'Mark as read',
+    'markUnread': 'Mark as unread',
+    'edit': 'Edit',
+    'delete': 'Delete',
+    'title': 'Title'
+  },
+
+  'save': 'Save',
+  'addFeed': 'Subscribe',
+  'logOut': 'Log out',
+  'next': 'Next',
+  'previous': 'Previous',
+  'allItems': 'All items',
+  'favorites': 'Favorites'
+};
---
+++ client/app/locales/fr/translations.js
@@ -0,0 +1,33 @@
+export default {
+  'feed': {
+    'markRead': 'Marquer comme lu',
+    'markUnread': 'Marquer comme non lu',
+    'edit': 'Modifier',
+    'delete': 'Supprimer',
+    'url': 'URL',
+    'folder': 'Dossier',
+    'reset': 'Réinitialiser le flux',
+    'pshb': {
+      'hub': 'Hub',
+      'topic': 'Sujet',
+      'expiration': 'Expiration',
+      'status': 'Statut'
+    }
+  },
+
+  'folder': {
+    'markRead': 'Marquer comme lu',
+    'markUnread': 'Marquer comme non lu',
+    'edit': 'Modifier',
+    'delete': 'Supprimer',
+    'title': 'Titre'
+  },
+
+  'save': 'Sauvegarder',
+  'addFeed': 'Abonnement',
+  'logOut': 'Déconnexion',
+  'next': 'Suivant',
+  'previous': 'Précédent',
+  'allItems': 'Tous les items',
+  'favorites': 'Favoris'
+};
